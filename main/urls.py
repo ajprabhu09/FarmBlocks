96d1876860fdcd42c1dd5588f2970db9bf33d4f0
ajinkya <ajinkyaprabhu97@gmail.com>
2019-03-10 23:55:50 +0530

    first commit

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'main'  # here for namespacing of urls.
+
+urlpatterns = [
+    path("", views.homepage, name="homepage"),
+    
+]
