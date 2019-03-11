778561a2251e0b809ee3ee5f379aef06097be445
ajinkya <ajinkyaprabhu97@gmail.com>
2019-03-11 05:57:08 +0530

    add LBC and file upload

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,9 @@ app_name = 'main'  # here for namespacing of urls.
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    
+   
+    # path("products/",views.homepage,name="homepage"),
+    # path("register/",views.homepage,name="homepage"),
+    # path("manf/",views.homepage,name="homepage"),
+    path("upload/",views.upload,name="upload")
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+import pybry
+from .forms import UploadFileForm
+
+lbry = pybry.LbryApi()
+balance_response = lbry.account_balance()
 # Create your views here.
 def homepage(request):
+	
+	
 	# return HttpResponse('HEllo')
-	return render(request,'main/index.html',context={})
+	print(balance_response[0])
+	if balance_response[1].status_code == 200:
+		return render(request,'main/index.html',context={'balance':balance_response[0]})
+	else:
+		return render(request,'main/index.html',context={'balance':-1})
+def products(request):
+	return render
+
+def upload(request):
+	if request.method == 'POST':
+		form = UploadFileForm(request.POST,request.FILES)
+		print(form.is_valid())
+		if form.is_valid():
+			print(request.FILES['file'])
+			print("Recieved FIL *************************")
+			return render(request,'main/index.html',context={'balance':balance_response[0]})
+		else:
+			form = UploadFileForm()
+			return render(request,'main/index.html',context={'balance':balance_response[0]})
+
+
+	else:
+		form = UploadFileForm()
+		return render(request,'main/upload.html',{'form':form})
